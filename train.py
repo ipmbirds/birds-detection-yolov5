fed1378d053900691ac22641354583f7913183ba
Reza Alidoost <reza_alidoost@hotmail.com>
2023-06-06 20:48:13 +0430

    Remove the print lines

--- train.py
+++ train.py
@@ -135,10 +135,6 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             x_shape,y_shape,z_shape, w_shape = csd_new['model.0.conv.weight'].shape
             for name, param in model.named_parameters():
                 if 'model.0.conv.weight' in name:
-                    print('-'*20)
-                    print(name)
-                    print('-'*20)
-                    print(param.shape)
                     with torch.no_grad():
                         param[:,0:3,:,:] = csd_new['model.0.conv.weight']
                         param[:,3: channels_num ,:,:] = torch.mean(csd_new['model.0.conv.weight'],1).reshape(x_shape,channels_num -3,z_shape,w_shape)
